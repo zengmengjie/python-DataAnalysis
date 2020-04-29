7d973235d06a397f039f3d0e91e8101fbd9121d4
zengmengjie <zengmengjie@gutou.com>
2020-04-29 11:33:39 +0800

    第一张：基础数据结构的构造以及基本方法

--- 326737/326737_1_string.py
+++ 326737/326737_1_string.py
@@ -1,4 +1,4 @@
-# 326736_1:常见的数据结构
+# 326736_1:常见的数据结构----字符串
 # 1.字符串构造
 # 单引号构造字符串:字符串中含有双引号时；
 string1 = '"commentTime":"2020-04-29","content":"包装良心！丰富多彩"'
@@ -39,6 +39,7 @@ else:
     print('男')
 
 # 字符串切片:按照固定长度，连续取出多个元素.可以用[start:end:step]表示。其中，start指定索引的起始位置；end指定索引的终止位置（注意，end位置的元素取不到！）；step指步长，默认为1，表示逐个取出子元素。
+# 有限切片
 price2 = '24.5元/500g'
 # 取出价格，并转换为浮点型
 print(float(price2[0:4]))
@@ -47,3 +48,56 @@ print(price2[6:len(price2)])
 ID = '123456198908187890'
 # 取出出生日期
 print(ID[6:14])
+
+# 无限切片
+price2 = '24.5元/500g'
+# 取出价格，并转换为浮点型
+print(float(price2[:4]))
+# 取出字符串中的“500g”
+print(price2[6:])
+print(price2[-4:])
+
+# 字符串查询
+price3 = '89.9元/桶'
+# 查询“元”所在的位置
+print(price3.index('元'))
+# 取出价格，并转换为浮点型
+print(float(price3[:price3.index('元')]))
+
+# 字符串压缩--去掉首位特殊字符信息
+prodName = '乒乓球拍（红双喜） '
+# 压缩右侧的空白字符
+print(prodName.rstrip())
+price3 = '单价：18.9 元/Kg'
+# 取出价格并转换为浮点数
+print(float(price3[3:price3.index('元')].rstrip()))
+sentence = '&&&^_^很喜欢，给满分！(＾－＾)'
+# 剔除评论中首尾的特殊字符
+print(sentence.strip('&^_^(＾－＾)'))
+
+# 字符串方法--替换
+# str.replace(old, new) old：指定被替换的子串； new：指定新的子串；
+sentence = '别克英朗1.3t的排量家用足够了，1.3T对应的可是163马力！'
+# 将小写的t替换为大写的T -- 按值替换
+print(sentence.replace('t', 'T'))
+tel = '13612347890'
+# 隐藏手机号中间四位
+print(tel.replace(tel[3:7], '****'))
+
+# 格式化插入“方法” str.format (values)  values：指定格式化的值；
+# 转换为格式化风格
+print('尊敬的{}{}，您的话费余额为{}元，请及时充值，以免影响通话！'.format('刘', '先生', 6.78))
+# 保留两位有效数字的格式化  ---:.2f ：标识了保留两位小数
+print('ROC曲线下的AUC值为：{:.2f}'.format(0.8356444))
+# 生成5个有规则的网页链接--{0}：指定对对应format里面的第一个参数 ，以此类推
+for month in [1, 2, 3, 4, 5]:
+    print('http://tianqi.2345.com/t/wea_history/js/20190{}/60008_20190{}.js'.format(month, month+1))
+
+# 分割方法 str.split(sep)  sep ：指定待分割的分割符；
+email = 'lsx1234567@163.com'
+# 将邮箱分割为邮箱名称和域名
+print(email.split('@'))
+info = '博佳花园 | 2室2厅 | 94.44平米 | 南 北 | 精装'
+# 取出二手房中的面积值，并转换为浮点型
+size = info.split('|')[2]
+print(float(size[:5].lstrip()))
